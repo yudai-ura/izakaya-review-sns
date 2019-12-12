f8d2af06bad351affa0a336b277a36630638b9a7
yudai.ura <yudai.ura@gmail.com>
2019-12-12 13:16:25 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  def index
+  end
+  
+  def show
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      flash[:success] = "新規ユーザー登録しました"
+      redirect_to root_url
+    else
+      flash[:danger] = "ユーザーの登録に失敗しました"
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destory
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  
+end
