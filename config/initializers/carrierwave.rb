d1f6741589a2e9fc5e2801ef3655a02a6e5eb8dd
yudai.ura <yudai.ura@gmail.com>
2020-01-25 03:13:43 +0000

    for heroku3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
-# equire 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   config.storage :fog
