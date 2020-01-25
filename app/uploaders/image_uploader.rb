a65b9907539d6a634b78284f96b25a6dae44b4b5
yudai.ura <yudai.ura@gmail.com>
2020-01-25 03:12:45 +0000

    for heroku2

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,7 +15,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
  
   # Choose what kind of storage to use for this uploader:
+  # if Rails.env.production?
   storage :fog
+# else
+  # storage :file
+# end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -23,6 +27,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
-equire 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+# equire 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   config.storage :fog
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    # aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: ENV['AWS_DEFAULT_REGION'],
     path_style: true
   }
