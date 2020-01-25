79b9e972ec0a81084450966cca1e0e8a6c2163b0
yudai.ura <yudai.ura@gmail.com>
2020-01-25 16:35:56 +0000

    css2

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,7 +7,7 @@ class Review < ApplicationRecord
   mount_uploader :image, ImageUploader
   
   
-  validates :title, presence: { message: "は必ず入力してください"}, length: { message: "20字以内で入力してください", maximum: 20}
+  validates :title, presence: { message: "は必ず入力してください"}, length: { message: "25字以内で入力してください", maximum: 25}
   validates :content, presence: { message: "は必ず入力してください"}, length: {message: "1000字以内で入力してください", maximum: 1000}
   validates :point, presence: { message: "は必ず入力してください" }, numericality: { message: "0から100(半角)の整数で入力してください", greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
   
